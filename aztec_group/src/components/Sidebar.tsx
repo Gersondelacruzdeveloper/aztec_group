e4b7ef81eb4e0987377c8b3f012cc95d83b8cb05
Gerson <gdelacruz.developer  @gmail.com>
2024-08-31 17:05:27 -0400

    update typescript Type, and add icons to sidebar

--- aztec_group/src/components/Sidebar.tsx
+++ aztec_group/src/components/Sidebar.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import '../assets/scss/Sidebar.scss';
+import pdfImg from '../assets/images/pdfImg.png';
+
+// Import icons from react-icons
+import { FaTachometerAlt, FaMoneyCheckAlt, FaFileAlt, FaBell } from 'react-icons/fa';
 
 const Sidebar: React.FC = () => {
   return (
     <div className="sidebar">
       <div className="logo">
-        <img src="pdf-logo.png" alt="PDF Logo" />
+        <img src={pdfImg} alt="PDF Logo" />
       </div>
       <ul>
-        <li>Dashbord</li>
-        <li>Transactions</li>
-        <li>Report</li>
-        <li>Notification</li>
+        <li>
+          <FaTachometerAlt className="icon" />
+          Dashboard
+        </li>
+        <li>
+          <FaMoneyCheckAlt className="icon" />
+          Transactions
+        </li>
+        <li>
+          <FaFileAlt className="icon" />
+          Report
+        </li>
+        <li>
+          <FaBell className="icon" />
+          Notification
+        </li>
       </ul>
     </div>
   );
